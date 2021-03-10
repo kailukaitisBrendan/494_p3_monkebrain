6f86105283ca140553809bb465eb2edfc7c0e272
bft <bft@umich.edu>
2021-03-10 15:50:27 -0500

    water destroys objects labeled 'Package' on trigger enter

---
+++ Assets/OnTriggerDestroy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTriggerDestroy : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hello1");
+
+        if (other.gameObject.CompareTag("Package"))
+        {
+            Debug.Log("Hello");
+            Destroy(other.gameObject);
+        }
+    }
+}
