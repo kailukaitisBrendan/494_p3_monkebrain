9b15ce1c6f50efb64056d7e2f076762ce98b79dd
brendankailukaitis <kailukb@umich.edu>
2021-03-12 21:38:25 -0500

    Added object pickup and dropoff.

---
+++ Assets/Scripts/Player/PickupObject.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupObject : MonoBehaviour
+{
+    public float pickupDistance;
+    public Transform itemSlot;
+    private bool _pickedUp;
+    private GameObject _pickedUpObject;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            // Drop item if we are already holding something. 
+            if (_pickedUp)
+            {
+                DropItem();
+            }
+            else
+            {
+                PickupItem();
+            }
+        }
+    }
+
+    private void DropItem()
+    {
+        // Re-enable rigidbody and collider.
+        Rigidbody rb = _pickedUpObject.GetComponent<Rigidbody>();
+        rb.isKinematic = false;
+        Collider col = _pickedUpObject.GetComponent<Collider>();
+        
+        // Set the parent back to null
+        _pickedUpObject.transform.parent = null;
+        col.enabled = true;
+
+        _pickedUp = false;
+        
+        //Re-enable collision.
+        Physics.IgnoreCollision(col, GetComponent<Collider>(), false);
+        
+        Debug.Log("Dropped Item");
+    }
+
+    private void PickupItem()
+    {
+        // Try to pick up item in front of the player.
+        // Create mask so we only collide with grabbable objects.
+        LayerMask mask = LayerMask.GetMask("Grabbable Object");
+        // Shoot a ray out a short distance in front of player. 
+        Vector3 pos = transform.position;
+        pos.y = itemSlot.position.y;
+        RaycastHit hit;
+        if (Physics.Raycast(pos, transform.TransformDirection(Vector3.forward), out hit, pickupDistance, mask))
+        {
+            Debug.DrawRay(pos, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Item picked up");
+        }
+        
+        _pickedUpObject = hit.transform.gameObject;
+        
+        // Get the rigidbody of our hit.
+        Rigidbody rb = _pickedUpObject.GetComponent<Rigidbody>();
+        //Disable the rigidbody and rest velocities 
+        rb.isKinematic = true;
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        
+        // Set it as the player.
+        _pickedUpObject.transform.SetParent(itemSlot);
+        // Reset the local position and rotation.
+        _pickedUpObject.transform.localPosition = Vector3.zero;
+        _pickedUpObject.transform.localEulerAngles = Vector3.zero;
+        
+        // Ignore collision between the collider and the player.
+        Collider col = _pickedUpObject.GetComponent<Collider>();
+        Physics.IgnoreCollision(col, GetComponent<Collider>(), true);
+
+        _pickedUp = true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Vector3 pos = transform.position;
+        pos.y = itemSlot.transform.position.y;
+        Vector3 direction = transform.TransformDirection(Vector3.forward) * pickupDistance;
+        Gizmos.DrawRay(pos, direction);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,281 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    float movementSpeed = 6f;
+    float jumpPower = 5f;
+    public LayerMask Climbable;
+    public Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newVelocity = rb.velocity;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector3 xzmove = cam.transform.forward;
+            xzmove.y = 0;
+            newVelocity = xzmove * movementSpeed;
+
+            
+
+
+            //Rotation
+            if (IsGrounded() && !Input.GetKey(KeyCode.Space) && newVelocity.y == 0)
+            {
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(newVelocity), 0.1f);
+            }
+            else
+            {
+                Vector3 newRot = newVelocity;
+                newRot.y = 0;
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(newRot), 0.1f);
+            }
+
+        }
+        
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            Vector3 xzmove = cam.transform.forward;
+            xzmove.y = 0;
+            newVelocity = xzmove * -movementSpeed;
+
+            //Rotation
+            if (IsGrounded() && !Input.GetKey(KeyCode.Space) && newVelocity.y == 0)
+            {
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(newVelocity), 0.1f);
+            }
+            else
+            {
+                Vector3 newRot = newVelocity;
+                newRot.y = 0;
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(newRot), 0.1f);
+            }
+        }
+        
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 xzmove = cam.transform.forward;
+            xzmove.y = 0;
+            Vector3 left = Vector3.Cross(xzmove, Vector3.up).normalized;
+            newVelocity = left * movementSpeed / 2;
+
+            //Rotation
+            if (IsGrounded() && !Input.GetKey(KeyCode.Space) && newVelocity.y == 0)
+            {
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(newVelocity), 0.1f);
+            }
+            else
+            {
+                Vector3 newRot = newVelocity;
+                newRot.y = 0;
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(newRot), 0.1f);
+            }
+
+        }
+        
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 xzmove = cam.transform.forward;
+            xzmove.y = 0;
+            Vector3 left = Vector3.Cross(xzmove, Vector3.up).normalized;
+            newVelocity = left * -movementSpeed / 2;
+
+            //Rotation
+            if (IsGrounded() && !Input.GetKey(KeyCode.Space) && newVelocity.y == 0)
+            {
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(newVelocity), 0.1f);
+            }
+            else
+            {
+                Vector3 newRot = newVelocity;
+                newRot.y = 0;
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(newRot), 0.1f);
+            }
+
+        }
+
+
+        // DIAGONAL MOVEMENT
+
+        if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
+        {
+            Vector3 xzmove = cam.transform.forward;
+            xzmove.y = 0;
+            
+            Vector3 left = Vector3.Cross(xzmove, Vector3.up).normalized;
+            newVelocity = (xzmove + left) * movementSpeed / 2;
+
+            //Rotation
+            if (IsGrounded() && !Input.GetKey(KeyCode.Space) && newVelocity.y == 0)
+            {
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(newVelocity), 0.1f);
+            }
+            else
+            {
+                Vector3 newRot = newVelocity;
+                newRot.y = 0;
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(newRot), 0.1f);
+            }
+        }
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
+        {
+            Vector3 xzmove = cam.transform.forward;
+            xzmove.y = 0;
+
+            Vector3 left = Vector3.Cross(xzmove, Vector3.up).normalized;
+            newVelocity = (xzmove - left) * movementSpeed / 2;
+            //Rotation
+            if (IsGrounded() && !Input.GetKey(KeyCode.Space) && newVelocity.y == 0)
+            {
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(newVelocity), 0.1f);
+            }
+            else
+            {
+                Vector3 newRot = newVelocity;
+                newRot.y = 0;
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(newRot), 0.1f);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+        {
+            Vector3 xzmove = cam.transform.forward;
+            xzmove.y = 0;
+
+            Vector3 left = Vector3.Cross(xzmove, Vector3.up).normalized;
+            newVelocity = (xzmove - left) * -movementSpeed / 2;
+            //Rotation
+            if (IsGrounded() && !Input.GetKey(KeyCode.Space) && newVelocity.y == 0)
+            {
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(newVelocity), 0.1f);
+            }
+            else
+            {
+                Vector3 newRot = newVelocity;
+                newRot.y = 0;
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(newRot), 0.1f);
+            }
+        }
+        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+        {
+            Vector3 xzmove = cam.transform.forward;
+            xzmove.y = 0;
+
+            Vector3 left = Vector3.Cross(xzmove, Vector3.up).normalized;
+            newVelocity = (xzmove + left) * -movementSpeed / 2;
+            //Rotation
+            if (IsGrounded() && !Input.GetKey(KeyCode.Space) && newVelocity.y == 0)
+            {
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(newVelocity), 0.1f);
+            }
+            else
+            {
+                Vector3 newRot = newVelocity;
+                newRot.y = 0;
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(newRot), 0.1f);
+            }
+        }
+
+
+
+        if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D))
+        {
+            newVelocity.x = 0;
+            newVelocity.z = 0;
+        }
+
+
+        //CLIMBING 
+
+
+        if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A)
+            || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)) && ExistsForwardWall())
+        {
+
+            newVelocity = transform.forward;
+            newVelocity.y = 3;
+            //Debug.Log("Hi");
+        }
+
+        //JUMP 
+
+        if (Input.GetKey(KeyCode.Space) && IsGrounded())
+        {
+           
+            newVelocity.y = jumpPower;
+
+
+        }
+
+        if (!IsGrounded() && !ExistsForwardWall())
+        {
+            newVelocity.y = rb.velocity.y;
+        }
+
+        rb.velocity = newVelocity;
+    }
+
+    public bool IsGrounded()
+    {
+        RaycastHit hit;
+        float dist = 1.01f;
+        Vector3 offset = new Vector3(0, 0, 0);
+        Debug.DrawRay(transform.position + offset, Vector3.down, Color.cyan);
+        if (Physics.Raycast(transform.position + offset, Vector3.down, out hit, dist))
+        {
+
+
+            return true;
+
+        }
+
+        return false;
+    }
+
+
+    public bool ExistsForwardWall()
+    {
+        RaycastHit hit;
+        float dist = 0.7f;
+        
+        Vector3 toes = new Vector3(0, -1f, 0);
+        Vector3 knees = new Vector3(0, -0.5f, 0);
+        Vector3 head = new Vector3(0, 1, 0);
+       
+
+
+        Debug.DrawRay(transform.position + toes, Vector3.forward, Color.cyan);
+        Debug.DrawRay(transform.position + knees, Vector3.forward, Color.cyan);
+        Debug.DrawRay(transform.position + head, Vector3.forward, Color.cyan);
+        
+
+        if (Physics.Raycast(transform.position + toes, transform.forward, out hit, dist, Climbable)
+            || Physics.Raycast(transform.position + knees, transform.forward, out hit, dist, Climbable)
+            || Physics.Raycast(transform.position + head, transform.forward, out hit, dist, Climbable)
+            )
+        {
+
+            return true;
+
+        }
+
+        return false;
+    }
+
+
+ 
+
+
+}
