238f60db4c939d3a44970566e7e0b92ad5989093
hoganmas <hoganmas@umich.edu>
2021-03-13 19:10:14 -0500

    Added Event Bus files

---
+++ Assets/Scripts/Event/Event.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event
+{
+}
+
+public class ButtonPressEvent
+{
+    public int id;
+
+    public ButtonPressEvent(int _id)
+    {
+        id = _id;
+    }
+}
