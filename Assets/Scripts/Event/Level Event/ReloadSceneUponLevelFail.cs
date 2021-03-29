6de1fcec65c6d8dba19d1a4b495f882b0ede1180
bft <bft@umich.edu>
2021-03-29 01:42:00 -0400

    sound effects done

--- Assets/Scripts/Event/Level Event/ReloadSceneUponLevelFail.cs
+++ Assets/Scripts/Event/Level Event/ReloadSceneUponLevelFail.cs
@@ -6,17 +6,29 @@ using UnityEngine.SceneManagement;
 public class ReloadSceneUponLevelFail : MonoBehaviour
 {
     private Subscription<LevelFailEvent> levelClearSubscription;
-
+    AudioSource sound;
+    public AudioClip stickemup;
     void Start()
     {
+        sound = GetComponent<AudioSource>();
         levelClearSubscription = EventBus.Subscribe<LevelFailEvent>(ReloadScene);
     }
 
     void ReloadScene(LevelFailEvent _event)
     {
+        sound.clip = stickemup;
+        sound.Play();
+        //Debug.Log("sound?");
+        StartCoroutine(WaitThenLoad());
+        
+    }
+
+
+    IEnumerator WaitThenLoad()
+    {
+        yield return new WaitForSeconds(1.5f);
         SceneManager.LoadScene(
             SceneManager.GetActiveScene().buildIndex
         );
     }
-
 }
