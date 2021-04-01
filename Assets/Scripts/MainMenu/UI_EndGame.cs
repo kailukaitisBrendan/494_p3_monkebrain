453de9d974af92613d890c22da6d92ee0e892a27
Jack <kije@umich.edu>
2021-04-01 08:02:13 -0400

    end game button

---
+++ Assets/Scripts/MainMenu/UI_EndGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_EndGame : MonoBehaviour
+{
+    public void OnButtonPress(){
+        Application.Quit();
+    }
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
