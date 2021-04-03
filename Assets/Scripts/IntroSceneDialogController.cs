c263e9f0c0d816b52fa78d57cca6fb30f2256fe7
Juliet Carpenter <julcarp@umich.edu>
2021-04-03 03:34:03 -0400

    playing with dialog for intro scene

---
+++ Assets/Scripts/IntroSceneDialogController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroSceneDialogController : MonoBehaviour
+{
+    public string[] messages;
+    RollInText rollInText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rollInText = GetComponent<RollInText>();
+        StartCoroutine(StartOldManSpeech());
+    }
+
+    IEnumerator StartOldManSpeech()
+    {
+        for (int i = 0; i < messages.Length; ++i)
+        {
+            rollInText.fullMessage = messages[i];
+            yield return StartCoroutine(rollInText.ShowText());
+            yield return new WaitForSeconds(3f);
+        }
+    }
+
+}
---
+++ Assets/Scripts/RollInText.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RollInText : MonoBehaviour
+{
+    TextMeshProUGUI textComponent;
+    public string fullMessage;
+    string currentMessage = "";
+    public float letterDelay = .1f;
+
+    private void Awake()
+    {
+        textComponent = this.GetComponent<TextMeshProUGUI>();
+
+    }
+    void Start()
+    {
+    }
+
+    public IEnumerator ShowText()
+    {
+        for (int i = 0; i < fullMessage.Length; ++i)
+        {
+            currentMessage += fullMessage[i];
+            textComponent.text = currentMessage;
+            yield return new WaitForSeconds(letterDelay);
+        }
+        currentMessage = "";
+    }
+}
