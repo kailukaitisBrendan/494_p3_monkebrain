2f203cfec4b951eba1f3a2f473cfc6700a41c07b
Jack <kije@umich.edu>
2021-04-03 10:49:19 -0400

    sliders

---
+++ Assets/Scripts/UI/SensSlider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SensSlider : MonoBehaviour
+{
+    void Start () {
+        PlayerPrefs.SetFloat("sens", 1f);
+    }
+
+    public void SetSens (float sliderValue)
+    {
+        PlayerPrefs.SetFloat("sens", sliderValue);
+    }
+}
