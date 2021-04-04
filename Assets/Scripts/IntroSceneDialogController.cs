0ecd4ec55a89f3a22920b1f6ef84e5ec1eb6d302
brendankailukaitis <kailukb@umich.edu>
2021-04-04 15:27:13 -0400

    Made duration public

--- Assets/Scripts/IntroSceneDialogController.cs
+++ Assets/Scripts/IntroSceneDialogController.cs
@@ -1,26 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class IntroSceneDialogController : MonoBehaviour
 {
     public string[] messages;
     RollInText rollInText;
+    public float textDuration = 3f;
+    
     // Start is called before the first frame update
     void Start()
     {
         rollInText = GetComponent<RollInText>();
+    }
+
+    public void OnCameraLive()
+    {
+        Debug.Log("Hello!");
         StartCoroutine(StartOldManSpeech());
     }
 
     IEnumerator StartOldManSpeech()
     {
+        //gameObject.SetActive(true);
         for (int i = 0; i < messages.Length; ++i)
         {
+            Debug.Log("Here");
             rollInText.fullMessage = messages[i];
             yield return StartCoroutine(rollInText.ShowText());
-            yield return new WaitForSeconds(3f);
+            yield return new WaitForSeconds(textDuration);
         }
+        
+        // Text is done.
+        //gameObject.SetActive(false);
     }
 
 }
