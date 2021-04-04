e01356c54afc4ddb64c195674c3c198025f6e4b7
Juliet Carpenter <julcarp@umich.edu>
2021-04-04 17:17:59 -0400

    baby cutscene change

--- Assets/Scripts/IntroSceneDialogController.cs
+++ Assets/Scripts/IntroSceneDialogController.cs
@@ -11,7 +11,7 @@ public class IntroSceneDialogController : MonoBehaviour
     int messagesIndex = 0;
     
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         rollInText = GetComponent<RollInText>();
     }
