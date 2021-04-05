861a10595ffbc1aebe1a4e0674874cc567f90fc8
hoganmas <root@DESKTOP-H4RI6OL.localdomain>
2021-04-05 00:01:58 -0400

    Fixed issue with blob shadow

---
+++ Assets/LockGlobalRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockGlobalRotation : MonoBehaviour
+{
+    private Quaternion initRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = initRotation;
+    }
+}
