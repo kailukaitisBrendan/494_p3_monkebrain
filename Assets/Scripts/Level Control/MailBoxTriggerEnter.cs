abbd58637ef486f013408a96c4318b0034bb0ef2
bft <bft@umich.edu>
2021-04-09 20:20:39 -0400

    new mailbox triggers win on package touch

---
+++ Assets/Scripts/Level Control/MailBoxTriggerEnter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MailBoxTriggerEnter : MonoBehaviour
+{
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 17)
+            EventBus.Publish<LevelClearEvent>(new LevelClearEvent());
+        if(other.gameObject.layer == 11)
+        {
+            
+            Transform gp = other.gameObject.transform.Find("ItemSlot").Find("Golden Package");
+            if (gp != null)
+            {
+                EventBus.Publish<LevelClearEvent>(new LevelClearEvent());
+            }
+        }
+    }
+}
