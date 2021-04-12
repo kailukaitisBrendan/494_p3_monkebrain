64ffb9a018d6ed2f5ba0b73cbf7476dff13a1b44
Jack <kije@umich.edu>
2021-04-12 02:26:43 -0400

    tumbleweed

---
+++ Assets/Scripts/hub/spawnTumbleweed.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnTumbleweed : MonoBehaviour
+{
+    float time_spawn;
+    float time_since;
+    public GameObject tumbleweed;
+    GameObject inst;
+    void Start() {
+        time_spawn = Time.time;
+        time_since = Time.time + 0.5f;
+    }
+    void FixedUpdate()
+    {
+        // at random intervals, spawn a new tumbleweed
+        if (Time.time - time_spawn > time_since) {
+            time_since = Time.time + Random.Range(1.0f,2.0f);
+            inst = Instantiate(tumbleweed, new Vector3 (transform.position.x - 30f,0.5f,transform.position.z + 30f), Quaternion.identity);
+            inst.GetComponent<tumbleweedMovement>().SetCam(gameObject);
+            inst = Instantiate(tumbleweed, new Vector3 (transform.position.x - 30f,0.5f,transform.position.z - 30f), Quaternion.identity);
+            inst.GetComponent<tumbleweedMovement>().SetCam(gameObject);
+        }
+    }
+}
--- Assets/Scripts/tumbleweedMovement.cs
+++ Assets/Scripts/tumbleweedMovement.cs
@@ -4,17 +4,40 @@ using UnityEngine;
 
 public class tumbleweedMovement : MonoBehaviour
 {
+    GameObject cam;
+    float time_spawn;
+    float time_run = 6.0f;
     float speed = 8f;
     float rot;
     void Start() {
         // generate random movement vector
         rot = Random.Range(-180f,180f);
         transform.rotation = Quaternion.Euler(0, rot, 0);
+        time_spawn = Time.time;
     }
 
     // Update is called once per frame
     void FixedUpdate() {
         // move in random direction
         transform.position += new Vector3(speed * Time.deltaTime * Mathf.Sin(rot),0f,speed * Time.deltaTime* Mathf.Cos(rot));
+        if (cam != null) {
+            if (Mathf.Abs(gameObject.transform.position.x - cam.transform.position.x) > 200f 
+            || Mathf.Abs(gameObject.transform.position.z - cam.transform.position.z) > 200f) {
+                Destroy(gameObject);
+            }
+        }
+        if (Time.time - time_spawn > time_run) {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider coll) {
+        if (coll.gameObject.layer != 12) {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetCam(GameObject came) {
+        cam = came;
     }
 }
