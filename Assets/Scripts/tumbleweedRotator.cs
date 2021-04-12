3f27606e80adc9193b9f10dac8eae989fcf27e47
Jack <kije@umich.edu>
2021-04-12 01:27:41 -0400

    tumbleweed

---
+++ Assets/Scripts/tumbleweedRotator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tumbleweedRotator : MonoBehaviour
+{
+    float rotat;
+    float speed = 500f;
+    void Start() {
+        rotat = 0.0f;
+    }
+    void FixedUpdate() {
+        rotat += speed * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(-rotat * Mathf.Sin(transform.rotation.y), 0, -rotat * Mathf.Cos(transform.rotation.y));
+    }
+}
