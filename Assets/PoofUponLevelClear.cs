aa19376662ca4e023cae2b17a1c5d0f5e13d0662
hoganmas <root@DESKTOP-H4RI6OL.localdomain>
2021-04-17 16:39:49 -0400

    Cleaned up Level 1-0

---
+++ Assets/PoofUponLevelClear.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoofUponLevelClear : MonoBehaviour
+{
+    Subscription<LevelClearEvent> levelClearSubscription;
+    public AudioClip poof_sound;
+    private AudioSource audio;
+    public ParticleSystem poof;
+
+    private void Awake() {
+        audio = GetComponent<AudioSource>();
+        levelClearSubscription = EventBus.Subscribe<LevelClearEvent>(_OnLevelClear);
+    }
+
+    void _OnLevelClear(LevelClearEvent e) {
+        // Disable MeshRenderers
+        MeshRenderer mr = null;
+        foreach (Transform child in transform) {
+            if (mr = child.GetComponent<MeshRenderer>()) {
+                mr.enabled = false;
+            }
+        }
+
+        // Disable Colliders
+        GetComponent<BoxCollider>().enabled = false;
+
+        if (poof_sound) {
+            audio.loop = false;
+            audio.PlayOneShot(poof_sound, 5f);
+        }
+        poof.Play();
+    }
+
+}
