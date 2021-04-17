7081186d38563971fce4f549776d84c74bb0ea0c
bft <bft@umich.edu>
2021-04-17 14:51:12 -0400

    can only trigger mailbox once

--- Assets/Scripts/Level Control/MailBoxTriggerEnter.cs
+++ Assets/Scripts/Level Control/MailBoxTriggerEnter.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class MailBoxTriggerEnter : MonoBehaviour
 {
-    
+    private bool triggeredOnce = false;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.layer == 17)
+        if (other.gameObject.layer == 17 && !triggeredOnce)
+        {
+            triggeredOnce = true;
             EventBus.Publish<LevelClearEvent>(new LevelClearEvent());
+            
+        }
         if(other.gameObject.layer == 11)
         {
             
@@ -17,8 +21,10 @@ public class MailBoxTriggerEnter : MonoBehaviour
             if (gp == null) {
                 gp = other.gameObject.transform.Find("ItemSlot").Find("Golden Package (1)");
             }
-            if (gp != null)
+            if (gp != null && !triggeredOnce)
             {
+                
+                triggeredOnce = true;
                 EventBus.Publish<LevelClearEvent>(new LevelClearEvent());
             }
         }
