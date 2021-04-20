4ab5b301d2f32b15be799387c1ec040acb594385
brendankailukaitis <kailukb@umich.edu>
2021-04-20 02:10:50 -0400

    Added bundle of golden packages to intro, Fixed raise Glass not following hand, fixed dancing animations not applying root motion, added new Gut Warmer material'

--- Assets/GlassRaiseMovement.cs
+++ Assets/GlassRaiseMovement.cs
@@ -1,50 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GlassRaiseMovement : MonoBehaviour
 {
-    Vector3 start;
-    Vector3 destination;
-    public GameObject startPos;
-    public GameObject endPos;
-    public float moveSpeed = 0.56f;
-    bool goingUp = true;
-    // Start is called before the first frame update
-    void Start()
+    public GameObject hand;
+
+    private Vector3 _offset;
+
+    private void Start()
     {
-        start = startPos.transform.position;
-        destination = endPos.transform.position;
+        // Calculate offset
+        _offset = transform.position - hand.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPos;
+       // Set the object's position to be the hand's position plus the offset
+       Vector3 newPosition = hand.transform.position + _offset;
 
-
-        if (transform.position.y > (destination.y - 0.01f))  StartCoroutine(WaitFalse());
-        else if (transform.position.y < (start.y + 0.01f)) StartCoroutine(WaitTrue());
-        
-
-        if (goingUp)
-            newPos = Vector3.MoveTowards(transform.position, destination, Time.deltaTime * moveSpeed);
-        else
-            newPos = Vector3.MoveTowards(transform.position, start, Time.deltaTime * moveSpeed);
-
-        transform.position = newPos;
+       transform.position = newPosition;
     }
 
-    IEnumerator WaitFalse()
-    {
-        yield return new WaitForSeconds(0.1f);
-        goingUp = false;
-    }
-
-    IEnumerator WaitTrue()
-    {
-        yield return new WaitForSeconds(0.1f);
-        goingUp = true;
-    }
+    // IEnumerator WaitFalse()
+    // {
+    //     yield return new WaitForSeconds(0.1f);
+    //     goingUp = false;
+    // }
+    //
+    // IEnumerator WaitTrue()
+    // {
+    //     yield return new WaitForSeconds(0.1f);
+    //     goingUp = true;
+    // }
 }
 
