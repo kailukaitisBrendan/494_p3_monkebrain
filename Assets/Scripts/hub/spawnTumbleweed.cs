4c2c3e960efd22919801f54a4d394ba649a4b88d
Jack <kije@umich.edu>
2021-04-21 15:23:46 -0400

    speed-change

--- Assets/Scripts/hub/spawnTumbleweed.cs
+++ Assets/Scripts/hub/spawnTumbleweed.cs
@@ -14,12 +14,11 @@ public class spawnTumbleweed : MonoBehaviour
     void Start() {
         Time.timeScale = 1.0f;
         time_spawn = Time.time;
-        time_since = 0.2f;
+        time_since = 2f;
     }
     void FixedUpdate() {
         // at random intervals, spawn a new tumbleweed
-        if (Time.time - time_spawn > time_since) {
-            time_since = Random.Range(0.8f,3.0f);
+        if (Time.time - time_spawn > time_since && Random.Range(0f,1f) > 0.95f) {
             time_spawn = Time.time;
             inst = Instantiate(tumbleweed, new Vector3 (transform.position.x - x_radius,y_coord,transform.position.z + z_radius), Quaternion.identity);
             inst.GetComponent<tumbleweedMovement>().SetCam(gameObject);
